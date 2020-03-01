29383f07438198095cc45ecb22175e446ec2df39
Clayton Watts <cletusw@gmail.com>
2020-02-29 23:54:42 -0800

    Add PanTilt stub

--- projects/joystick_rvr/main.py
+++ projects/joystick_rvr/main.py
@@ -1,13 +1,18 @@
 import asyncio
-from rvr import Rvr
 from gamepad import Gamepad
+from pan_tilt import PanTilt
+from rvr import Rvr
 
 loop = asyncio.get_event_loop()
 rvr = Rvr(loop)
 gamepad = Gamepad('/dev/input/event0')
+panTilt = PanTilt()
 
 async def main():
-    await asyncio.gather(gamepad.runLoop(), rvr.runLoop(gamepad))
+    await asyncio.gather(gamepad.runLoop(),
+        rvr.runLoop(gamepad),
+        panTilt.runLoop(gamepad)
+    )
 
 if __name__ == '__main__':
     try:
---
+++ projects/joystick_rvr/pan_tilt.py
@@ -0,0 +1,6 @@
+class PanTilt:
+    def __init__(self):
+        pass
+    
+    async def runLoop(self, gamepad):
+        return
