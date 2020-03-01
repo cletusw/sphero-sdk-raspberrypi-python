db7114e5041a07b5a36e73a45913534cec9b48af
Clayton Watts <cletusw@gmail.com>
2020-02-29 17:48:25 -0800

    Gamepad class

--- projects/joystick_rvr/gamepad.py
+++ projects/joystick_rvr/gamepad.py
@@ -6,68 +6,56 @@ EVDEV_MAX = 65535
 EVDEV_CENTER = EVDEV_MAX / 2
 JOYSTICK_MAX = EVDEV_CENTER
 
-devicePath = '/dev/input/event0'
-gamepad: InputDevice = None
+class Gamepad:
 
-x = 0 # -JOYSTICK_MAX -> JOYSTICK_MAX
-y = 0
+    def __init__(self, devicePath):
+        self.devicePath = devicePath
+        self.reset()
 
-def resetGamepad():
-    global gamepad
-    global x
-    global y
+    def reset(self):
+        self.gamepad: InputDevice = None
+        self.x = 0 # -JOYSTICK_MAX -> JOYSTICK_MAX
+        self.y = 0
 
-    gamepad = None
-    x = 0
-    y = 0
+    def log(self):
+        print(f'x: {self.x}, y: {self.y}')
 
-def logJoystick():
-    print(f'x: {x}, y: {y}')
+    async def waitForGamepad(self):
+        if self.gamepad:
+            return
 
-async def waitForGamepad():
-    global gamepad
+        if not path.exists(self.devicePath):
+            print("Waiting for gamepad...")
 
-    if gamepad:
-        return
-
-    if not path.exists(devicePath):
-        print("Waiting for controller...")
-
-    while not path.exists(devicePath):
-        await asyncio.sleep(0.1)
-
-    while True:
-        try:
-            gamepad = InputDevice(devicePath)
-            print(gamepad)
-            break
-        except PermissionError:
+        while not path.exists(self.devicePath):
             await asyncio.sleep(0.1)
 
-def handleEvent(event):
-    global x
-    global y
+        while True:
+            try:
+                self.gamepad = InputDevice(self.devicePath)
+                print("Found: ", self.gamepad)
+                break
+            except PermissionError:
+                await asyncio.sleep(0.1)
 
-    if event and event.type == ecodes.EV_ABS:
-        #print("Got event")
-        if event.code == ecodes.ABS_X:
-            x = event.value - EVDEV_CENTER
-        elif event.code == ecodes.ABS_Y:
-            y = event.value - EVDEV_CENTER
+    def handleEvent(self, event):
+        if event and event.type == ecodes.EV_ABS:
+            #print("Got event")
+            if event.code == ecodes.ABS_X:
+                self.x = event.value - EVDEV_CENTER
+            elif event.code == ecodes.ABS_Y:
+                self.y = event.value - EVDEV_CENTER
 
-async def getJoystickValues():
-    while True:
-        try:
-            await waitForGamepad()
+    async def runLoop(self):
+        while True:
+            try:
+                await self.waitForGamepad()
 
-            async for event in gamepad.async_read_loop():
-                handleEvent(event)
-                #logJoystick()
-                yield (x, y)
+                async for event in self.gamepad.async_read_loop():
+                    self.handleEvent(event)
+                    #self.log()
 
-        except OSError as e:
-            print('Lost controller', e)
-            resetGamepad()
-            yield (x, y)
-
+            except OSError as e:
+                print('Gamepad disconnected:', e)
+                self.reset()
 
--- projects/joystick_rvr/main.py
+++ projects/joystick_rvr/main.py
@@ -3,7 +3,7 @@ from os import path
 import sys
 from mock import AsyncMock
 from raw_drive import drive
-from gamepad import JOYSTICK_MAX, getJoystickValues
+from gamepad import JOYSTICK_MAX, Gamepad
 
 sys.path.append(path.abspath(path.join(path.dirname(__file__), '../../')))
 from sphero_sdk import SerialAsyncDal, SpheroRvrAsync, RawMotorModesEnum
@@ -15,8 +15,7 @@ MIN_SPEED = 40
 
 RVR_FRAME_PERIOD_SEC = 0.01
 
-x = 0 # -JOYSTICK_MAX -> JOYSTICK_MAX
-y = 0
+gamepad = Gamepad('/dev/input/event0')
 
 loop = asyncio.get_event_loop()
 try:
@@ -25,35 +24,20 @@ try:
             loop
         )
     )
-    print("Connected to RVR")
 except asyncio.TimeoutError:
     print("Timed out waiting for SpheroRvrAsync. Using mock instead.")
     rvr = AsyncMock(SpheroRvrAsync)
 
-def logJoystick():
-    print(f'x: {x}, y: {y}')
-
-async def handleJoystick():
-    global x
-    global y
-
-    async for values in getJoystickValues():
-        x, y = values
-        #logJoystick()
-
 async def handleRvr():
-    global x
-    global y
-
     await rvr.wake()
     await rvr.reset_yaw()
 
     while True:
-        await drive(x, y, rvr, MIN_SPEED, MAX_SPEED, DEADBAND_RADIUS, JOYSTICK_MAX)
+        await drive(gamepad.x, gamepad.y, rvr, MIN_SPEED, MAX_SPEED, DEADBAND_RADIUS, JOYSTICK_MAX)
         await asyncio.sleep(RVR_FRAME_PERIOD_SEC)
 
 async def main():
-    await asyncio.gather(handleJoystick(), handleRvr())
+    await asyncio.gather(gamepad.runLoop(), handleRvr())
 
 if __name__ == '__main__':
     try:
