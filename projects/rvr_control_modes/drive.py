0ed2a6735d18d28168eb9477e21fb351ef4e510a
Clayton Watts <cletusw@gmail.com>
2020-10-04 15:30:44 -0700

    First try: decrease max angular speed as linear speed increases

--- projects/rvr_control_modes/drive.py
+++ projects/rvr_control_modes/drive.py
@@ -18,11 +18,22 @@ async def drive(x, y, z, rz, rvr, MIN_SPEED, MAX_SPEED, DEADBAND_RADIUS, MAX_MAG
 
         return normalizedValue if isPositive else -normalizedValue
 
-    # print(y, normalize(y), z, normalize(z))
+    linear_velocity = -normalize(y, MIN_SPEED, MAX_SPEED)
+
+    # Calculate max angular speed that decreases as linear speed increases
+    linear_speed = abs(linear_velocity)
+    max_angular_speed = int(MAX_SPEED * interp(
+        linear_speed,
+        [0, MAX_SPEED],
+        [0.7, 0.4],
+    ))
+
+    yaw_angular_velocity = -normalize(x, MIN_SPEED, max_angular_speed)
+    # print(y, linear_velocity, x, yaw_angular_velocity)
 
     # Valid linear & angular velocity values are -127..127
     await rvr.drive_rc_normalized(
-        linear_velocity = -normalize(y, MIN_SPEED, MAX_SPEED),
-        yaw_angular_velocity = -normalize(z, MIN_SPEED, MAX_SPEED),
+        linear_velocity = linear_velocity,
+        yaw_angular_velocity = yaw_angular_velocity,
         flags = 0,
     )
